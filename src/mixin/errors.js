b4bbd491d75df9dd24bc46b847efc8f9245abcba
Gunes Can Capanoglu <16815827+gunescancapanoglu@users.noreply.github.com>
2019-09-30 13:18:44 +0300

    . quasar 1.1.6 update . small fix to errors import, to get rid of parameter unused error . added not found 404 error with url forwarding

--- src/mixin/errors.js
+++ src/mixin/errors.js
@@ -3,11 +3,15 @@ export const errors = {
     connectionError(err) {
       this.$q.loading.hide();
       this.$router.push("/error/firestore");
-      return Promise.reject(new Error("Connection problem with firestore servers with error:" + err));
+      return Promise.reject(new Error("Connection problem with firestore servers with error:" + err ? err : undefined));
     }, cdnProblem(err) {
       this.$q.loading.hide();
       this.$router.push("/error/cdn");
-      return Promise.reject(new Error("Connection problem with cdn servers."));
+      return Promise.reject(new Error("Connection problem with cdn servers. Object: " + err ? err : undefined));
+    }, notFound(err) {
+      this.$q.loading.hide();
+      this.$router.push("/404");
+      return Promise.reject("Requestd content could not found. Url is probably wrongly typed. Object: " + err ? err : undefined);
     }
   }
 };
